3f8255c4cb717131878e08b180e85e4f260ff896
Sergio Pedraza Vázquez <spedvaz@etsinf.upv.es>
2024-10-15 00:03:20 +0200

    Implementacion de ListArray añadida

--- ListArray.h
+++ ListArray.h
@@ -14,7 +14,6 @@ class ListArray : public List<T> {
     ListArray();
 	~ListArray();
 	T operator[](int pos);
-<<<<<<< HEAD
 	template <typename U>
 	friend std::ostream& operator<<(std::ostream &out, const ListArray<U> &list);
 	void insert(int pos, T e) override final;
@@ -37,29 +36,6 @@ ListArray<T>::ListArray(){
 	this->max = MINSIZE;
 	this->n = 0;
 }
-=======
-	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
-	void insert(int pos, T e) const override final;
-	void append(T e) const override final;
-	void prepend(int pos) const override final;		
-	T remove(int pos) const override final;		
-	T get(int pos) const override final;
-	int search(T e) const override final;		
-	bool empty() const override final;		
-	int size() const override final;		
-};
-
-template <typename T>
-
-ListArray<T>::ListArray(){
-	T* arr;
-	int max;
-	int n;
-	static const int MINSIZE;
-	arr = new T [MINSIZE];i
-}
-
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 template <typename T>
 
 ListArray<T>::~ListArray(){
@@ -68,41 +44,24 @@ ListArray<T>::~ListArray(){
 
 template <typename T>
 
-<<<<<<< HEAD
 T ListArray<T>::operator[](int pos){
 	if(pos < 0 || pos > size()-1){
 		throw std::out_of_range("Esta fuera del intervalo");
 	}
 	return arr[pos];
-=======
-ListArray<T>::operator[](int pos){
-	if(pos < 0 || pos > ListArray::size()-1){
-		throw std::out_of_range("Esta fuera del intervalo");
-	}
-	return pos;
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 }
 
 template <typename T>
 
-<<<<<<< HEAD
 std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
 	for( int i = 0; i < list.n ; i++){
 		out << list.arr[i] << " ";
 	}
 	return out;
-=======
-ListArray::std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
-	for( int i = 0; i < n ; i++){
-		out << arr[i] << " ";
-		return out;
-	}
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 }
 
 template <typename T>
 
-<<<<<<< HEAD
 void ListArray<T>::resize(int new_size){
 	T* new_arr = new T[new_size];
 	for(int i = 0;i < n; i++){
@@ -116,38 +75,25 @@ void ListArray<T>::resize(int new_size){
 template <typename T>
 
 void ListArray<T>::insert(int pos, T e){
-=======
-void ListArray<T>::insert (int pos, T e){
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 	if(pos < 0 || pos > size()){
 		throw std::out_of_range("No esta en rango");
 	}else{
 		ListArray<T>::resize(max+1);
-<<<<<<< HEAD
 		for(int i = max; i > pos; i--){
 			arr[i] = arr[i-1];
 		}
 		arr[pos] = e;
-=======
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 	}
 }
 
 template <typename T>
 
 void ListArray<T>::append(T e){
-<<<<<<< HEAD
 	insert(max+1, e);  
-=======
-	
-	insert(n, T e);  
-	n++;
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 }
 
 template <typename T>
 
-<<<<<<< HEAD
 void ListArray<T>::prepend(T e){
 	insert( 0, e);
 }
@@ -163,42 +109,16 @@ T ListArray<T>::remove(int pos){
 		resize(max-1);
 		return valor;
 	}
-=======
-void ListArray<T>prepend(T e){
-	for(int i = 0; i < size; i++){
-		data[i+1] = data[i];
-	}
-	insert( 0 , T e);
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 }
 
 template <typename T>
 
-<<<<<<< HEAD
 T ListArray<T>::get(int pos){
 	if( pos < 0 || pos > size-1){
 		throw std::out_of_range("La posición está fuera de rango");
 	}else{
 		return arr[pos];
 	}
-=======
-ListArray::T remove(int pos){
-	if( pos < 0 || pos > size-1){
-		throw std::out_of_range("La posición está fuera de rango");
-	}
-	int valor = arr[pos];
-	delete[] arr[pos];//si se elimina como puedo reservarlo
-	return valor;
-}
-
-template <typename T>
-
-ListArray::T get(int pos){
-	if( pos < 0 || pos > size-1){
-		throw std::out_of_range("La posición está fuera de rango");
-	}
-	return arr[pos];
->>>>>>> 1a57e8c6a1ed493323181b3799558a048ab609aa
 }
 
 template <typename T>
@@ -227,3 +147,4 @@ template <typename T>
 int ListArray<T>::size(){
 	return n;
 }
+
